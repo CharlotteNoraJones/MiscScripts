1cc5b5e390591133a39369de00c83c6ef80c892c
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-05-30 11:33:24 -0500

    Created Project

---
+++ C#/BookstoreApp/BookStoreApp.API/Data/CodeFirstTable.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApp.API.Data
+{
+    public class CodeFirstTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
